5d541a4fc2bee1804065c7b60ff9998c6352a82d
Esmail <esmailelhanash@gmail.com>
2024-09-10 16:30:42 +0300

    fixed TORCH light. added intent feature to service. added notification permission. upgraded agp and gradle.

--- app/src/main/java/com/esmailelhanash/sonicflash/presentation/PermissionsHandler.kt
+++ app/src/main/java/com/esmailelhanash/sonicflash/presentation/PermissionsHandler.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -20,7 +21,13 @@ object PermissionsHandler {
         arrayOf(
             Manifest.permission.CAMERA,
             Manifest.permission.FOREGROUND_SERVICE_CAMERA,
+            Manifest.permission.POST_NOTIFICATIONS,
             Manifest.permission.FOREGROUND_SERVICE,)
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.POST_NOTIFICATIONS
+        )
     } else {
         arrayOf(
             Manifest.permission.CAMERA,
@@ -29,18 +36,22 @@ object PermissionsHandler {
 
     @Composable
     fun RequestMissingPermissions(a: Activity, onAllPermissionsGranted: () -> Unit) {
-        getMissingPermissions(a).forEach {
-            cameraPermissionLauncher(a, it){
+        getMissingPermissions(a).let {
+            val launcher = cameraPermissionLauncher(a){
                 // if all requiredPermissions are granted
                 if (checkPermissions(a))
                     onAllPermissionsGranted()
-            }.launch(it)
+            }
+            LaunchedEffect(launcher) {
+                launcher.launch(it)
+            }
+
         }
     }
 
     private fun getMissingPermissions(a: Activity) = requiredPermissions.filter {
         ContextCompat.checkSelfPermission(a, it) != PackageManager.PERMISSION_GRANTED
-    }
+    }.toTypedArray()
 
     fun checkPermissions(a: Activity): Boolean
         = requiredPermissions.none {
@@ -51,35 +62,37 @@ object PermissionsHandler {
     @Composable
     private fun cameraPermissionLauncher(
         context: Context,
-        permission: String,
         onPermissionsGranted: (permission: String) -> Unit
     ) = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (!isGranted) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    context as Activity,
-                    permission
-                )
-            ) {
-                Toast.makeText(
-                    context,
-                    "Camera permissions is needed to access the flashlight.",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else {
-                Toast.makeText(
-                    context,
-                    "Camera Permissions Denied Permanently. Go to settings to enable it.",
-                    Toast.LENGTH_LONG
-                ).show()
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                    data = Uri.fromParts("package", context.packageName, null)
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { areGranted: Map<String, @JvmSuppressWildcards Boolean> ->
+        areGranted.forEach { (permission, isGranted) ->
+
+            if (!isGranted) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        context as Activity,
+                        permission
+                    )
+                ) {
+                    Toast.makeText(
+                        context,
+                        "Camera permissions is needed to access the flashlight.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Camera Permissions Denied Permanently. Go to settings to enable it.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = Uri.fromParts("package", context.packageName, null)
+                    }
+                    context.startActivity(intent)
                 }
-                context.startActivity(intent)
+            } else {
+                onPermissionsGranted(permission)
             }
-        }else {
-            onPermissionsGranted(permission)
         }
     }
 }
--- app/src/main/java/com/esmailelhanash/sonicflash/service/FlashLightService.kt
+++ app/src/main/java/com/esmailelhanash/sonicflash/service/FlashLightService.kt
@@ -3,6 +3,7 @@ package com.esmailelhanash.sonicflash.service
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Binder
 import android.os.Build
@@ -17,7 +18,9 @@ import com.esmailelhanash.sonicflash.R
 import com.esmailelhanash.sonicflash.data.camera.CameraManager
 import com.esmailelhanash.sonicflash.data.flashlight.FlashLightController
 import com.esmailelhanash.sonicflash.data.model.FlashEffect
+import com.esmailelhanash.sonicflash.data.model.defaultFlashModes
 import com.esmailelhanash.sonicflash.domain.camera.ICameraProvider
+import com.esmailelhanash.sonicflash.presentation.ui.MainActivity
 import com.esmailelhanash.sonicflash.presentation.viewmodel.FlashlightViewModel
 import com.esmailelhanash.sonicflash.presentation.viewmodel.FlashlightViewModelFactory
 
@@ -27,16 +30,12 @@ class FlashlightService : LifecycleService(), ViewModelStoreOwner {
     private lateinit var cameraProvider : ICameraProvider
     private val binder = LocalBinder()
 
+    // toggle flash action
+    private val toggleAction = "toggle"
+
     // FlashlightViewModel instance
     private lateinit var flashlightViewModel : FlashlightViewModel
-        // getter with exception check
-//   fun getFlashlightViewModel() : FlashlightViewModel? {
-//        return if (::flashlightViewModel.isInitialized) {
-//            flashlightViewModel
-//        } else {
-//            null
-//        }
-//    }
+
     override fun onBind(intent: Intent): IBinder {
         super.onBind(intent)
         return binder
@@ -55,15 +54,12 @@ class FlashlightService : LifecycleService(), ViewModelStoreOwner {
         }
         startForeground(1, createNotification())
         initializeCameraManager()
-        initializeFlashManager()
 
         flashlightViewModel = FlashlightViewModelFactory(
             FlashLightController(cameraProvider)
         ).create(FlashlightViewModel::class.java)
 
-    }
-
-    private fun initializeFlashManager() {
+        flashlightViewModel.setFlashEffect(defaultFlashModes.first())
 
     }
 
@@ -72,6 +68,14 @@ class FlashlightService : LifecycleService(), ViewModelStoreOwner {
     }
 
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        // if toggle command, toggle the flash
+        if (intent?.action == toggleAction) {
+            toggleFlash()
+        }
+        return START_STICKY
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
@@ -83,9 +87,32 @@ class FlashlightService : LifecycleService(), ViewModelStoreOwner {
     private fun createNotification(): Notification {
         val builder = NotificationCompat.Builder(this, notificationChannelId)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("My Service")
-            .setContentText("Service is running")
+            .setContentTitle("SonicFlash")
+            .setContentText("SonicFlash is running")
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .addAction(
+                R.drawable.ic_launcher_foreground,  // Replace with your icon
+                "Toggle Flashlight",
+                PendingIntent.getService(
+                    this,
+                    0,
+                    Intent(this, FlashlightService::class.java).apply {
+                        setAction(toggleAction)
+                    },
+                    PendingIntent.FLAG_MUTABLE
+                )
+            )
+            .setContentIntent(
+                // MainActivity intent
+                PendingIntent.getActivity(
+                    this,
+                    0,
+                    Intent(this, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    },
+                    PendingIntent.FLAG_MUTABLE
+                )
+            )
         // Add actions or other customizations as needed
 
         return builder.build()
